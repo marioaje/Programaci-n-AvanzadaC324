ed9db4c9c35b4b0a801abca7bd378082854567ee
marioaje <marioaje@gmail.com>
2024-10-11 18:24:09 -0600

    ejercicio 1

---
+++ Ejercicios/Ejercicios/Pages/Profe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ejercicios.Pages
+{
+    public class ProfeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
