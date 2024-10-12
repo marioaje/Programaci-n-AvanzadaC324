29dc55dbb23fdb6bc0c8ef2d8c375cef2a3d5706
marioaje <marioaje@gmail.com>
2024-10-11 20:49:43 -0600

    v4
    Fin del api

--- API/API/Controllers/ProductoController.cs
+++ API/API/Controllers/ProductoController.cs
@@ -29,9 +29,74 @@ namespace API.Controllers
             /// {
             //    "TV","Itachi","Venom","laptop","monitor"
             //};
-            return Ok(id);
+
+            if (id >= _productos.Count || id < 0)
+            {
+                return NotFound("El id no esta en el sistema.");
+            }
+
+            return Ok(_productos[id]);
         }
-        //GET POST PUT DELETE CRUD
+        //GET
+        //
+        //
+        //
+        //POST PUT DELETE CRUD
+        [HttpPost]
+        public IActionResult AgregarProducto([FromBody] string _nuevoProducto)
+        {
+
+            //????
+            if (string.IsNullOrWhiteSpace(_nuevoProducto) ||
+                 string.IsNullOrEmpty(_nuevoProducto))
+            {
+                return BadRequest("El nombre no es valido.");
+            }
+
+            _productos.Add(_nuevoProducto);
+            return CreatedAtAction(
+                nameof(ObtenerProductos),
+                new { id = _productos.Count - 1 }
+            );
+        }
+
+        //DELETE???
+        [HttpDelete("{id}")]
+        public IActionResult BorrarProductos(int id)
+        {
+
+            if (id >= _productos.Count || id < 0)
+            {
+                return NotFound("El id no esta en el sistema.");
+            }
+            _productos.RemoveAt(id);
+
+            return NoContent();
+        }
+
+        //PUT???
+
+        [HttpPut("{id}")]
+        public IActionResult ActualizarProducto(int id, [FromBody] string _nuevoProducto)
+        {
+
+            if (id >= _productos.Count || id < 0)
+            {
+                return NotFound("El id no esta en el sistema.");
+            }
+
+            if (string.IsNullOrWhiteSpace(_nuevoProducto) ||
+                string.IsNullOrEmpty(_nuevoProducto))
+            {
+                return BadRequest("El nombre no es valido.");
+            }
+
+            //Actualizar
+            _productos[id] = _nuevoProducto;
+
+
+            return Ok($"Producto modificado, id: {id} , {_productos[id]} ");
+        }
 
 
     }
