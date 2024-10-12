78a1e0f30ac0a725c7c1252e51e8bce7aa8f8b8f
marioaje <marioaje@gmail.com>
2024-10-11 19:13:13 -0600

    API
    API

---
+++ API/API/Controllers/ProductoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObtenerPrdocutos()
+        {
+            return Ok("Saludos clase");
+        }
+        //GET POST PUT DELETE CRUD
+
+
+    }
+}
